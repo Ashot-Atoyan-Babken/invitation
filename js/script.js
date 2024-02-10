75a52afb0c7d0db937ea4b3737b79f12ea80dd40
Ashot <ashot-atoyan@mail.ru>
2024-02-10 17:09:24 +0400

    update

--- js/script.js
+++ js/script.js
@@ -1,9 +1,7 @@
 let run = document.querySelector('.run');
 run.addEventListener('mouseover', () => {
-  run.style.top = Math.floor(Math.random() * 256) + 'px';
-  run.style.left = Math.floor(Math.random() * 256) + 'px';
-  run.style.right = Math.floor(Math.random() * 256) + 'px';
-  run.style.bottom = Math.floor(Math.random() * 256) + 'px';
+  run.style.top = Math.floor(Math.random() * 250) + 'px';
+  run.style.bottom = Math.floor(Math.random() * 250) + 'px';
   run.style.position = 'relative';
 });
 
